03e28b19d74647ac0ded39fc95d4c55846f91683
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-07 21:14:21 +0000

    added password hash, validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
