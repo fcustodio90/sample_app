61197f83c1ad88311561928b94e2488016f4780a
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-07 02:23:37 +0000

    refactored tests and added helpers for titles in each static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,25 +9,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     # :success is the equivalent of HTTP status code 200 - OK
     assert_response :success
-    assert_select 'title', @base_title.to_s
+    assert_select 'title', "Home | #{@base_title}"
   end
 
   test 'should get help' do
     get static_pages_help_url
     # :success is the equivalent of HTTP status code 200 - OK
     assert_response :success
-    assert_select 'title', @base_title.to_s
+    assert_select 'title', "Help | #{@base_title}"
   end
 
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', @base_title.to_s
+    assert_select 'title', "About | #{@base_title}"
   end
 
   test 'should get contact' do
     get static_pages_contact_url
     assert_response :success
-    assert_select 'title', @base_title.to_s
+    assert_select 'title', "Contact | #{@base_title}"
   end
 end
