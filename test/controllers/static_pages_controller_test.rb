ea45ff2208adf29b32a62a3e0a9e65fc9db71f1f
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-07 00:36:40 +0000

    Add a full_titler helpder

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,36 +5,29 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test 'should get root' do
-    get root_url
-    assert_response :success
-    assert_select 'title', "Home | #{@base_title}"
-  end
-
   test 'should get home' do
     get static_pages_home_url
     # :success is the equivalent of HTTP status code 200 - OK
     assert_response :success
-    assert_select 'title', "Home | #{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
 
   test 'should get help' do
     get static_pages_help_url
     # :success is the equivalent of HTTP status code 200 - OK
     assert_response :success
-    assert_select 'title', "Help | #{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
 
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
 
   test 'should get contact' do
     get static_pages_contact_url
     assert_response :success
-    assert_select 'title', "Contact | #{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
-
 end
