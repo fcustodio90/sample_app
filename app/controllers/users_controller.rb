de282955f6e4855d39f7f142d875b466bf78585c
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-13 01:26:03 +0000

    signup tests and custom flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # handle a successful save.
+      flash[:success] = "Welcome to the Sample App! :)"
+      redirect_to @user
     else
       render 'new'
     end
