a6a47e4b0a2ee6e0cda9f90086d32fa38d08be97
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-15 22:42:34 +0000

    finished basic login, finalized tests and added destroy session

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -21,11 +21,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test 'login with valid information' do
+  test 'login with valid information followed by logout' do
     # 1- Visit the login path
     get login_path
     # 2- Post valid information to the sessions path
     post login_path params: { session: { email: @user.email, password: 'password'} }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -35,5 +36,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', logout_path
     # 5- Verify that a profile link appears
     assert_select 'a[href=?]', user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
