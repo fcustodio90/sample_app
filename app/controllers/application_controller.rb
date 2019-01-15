ddc9fe91e096bc034a7f5c663724910d2dc04d75
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-15 22:02:43 +0000

    more helpers for sessions, bootstrap for dropdown menus with conditional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  include SessionsHelper
+  # Logs in the given user
   def hello
     render html: "hello, world!"
   end
