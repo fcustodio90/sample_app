28793c41e86f99407a0a405ab6153756d7b912bf
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-18 01:37:40 +0000

    more helpers for sessions and added remember user to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+      remember user
       redirect_to user
     else
       # Create an error message.
