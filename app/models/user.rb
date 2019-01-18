28d50ac6e043c9e80ea7acdbbf0a25840c49a6a1
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-18 01:39:23 +0000

    forget method for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,10 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  # Forgets a user
+
+  def forget
+    update_attribute[:remember_digest, nil]
+  end
 end
