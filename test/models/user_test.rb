6f58fd2b0ffb5408e11bc80ab11c96a8be88883f
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-18 01:53:55 +0000

    fix to user model for authenticated method to return nil if user is logged out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,4 +70,8 @@ class UserTest < ActiveSupport::TestCase
   test 'password should have a minimum length' do
     @user.password = @user.password_confirmation = 'a' * 5
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
