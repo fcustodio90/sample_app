0b0930f601da172db85502114b2c80f5a4dc6f95
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-18 16:11:16 +0000

    tests for users edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'unsucessful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: {name: '',
+                                             email: 'foo@invalid',
+                                             password: 'foo',
+                                             password_confirmation: 'bar' } }
+    assert_template 'users/edit'
+  end
+
+  test 'invalid edit information' do
+    get edit_user_path(@user)
+    assert_no_difference 'User.count' do
+    patch user_path(@user), params: { user: { name: '',
+                                         email: 'foo@invalid',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
+    end
+    assert_template 'users/edit'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
 end
